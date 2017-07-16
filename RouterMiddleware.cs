fa465a9819d40b89314ec8d457346ad9ed72695e
Laban Eilers <laban@eilers.com>
2017-07-16 14:02:09 -0400

    basic proxy working for text

--- RouterMiddleware.cs
+++ RouterMiddleware.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
@@ -17,8 +18,43 @@ namespace Splicr
         {
             Console.WriteLine($"Request for {httpContext.Request.Path} received ({httpContext.Request.ContentLength ?? 0} bytes)");
             
+            try
+            {
+                var httpClient = new HttpClient();
+
+                string pathAndQuery = httpContext.Request.Path + httpContext.Request.QueryString;
+
+                httpClient.DefaultRequestHeaders.Clear();
+                foreach (var pair in httpContext.Request.Headers) 
+                {
+                    if (pair.Key.Equals("host", StringComparison.OrdinalIgnoreCase)) {
+                        continue;
+                    }
+
+                    Console.WriteLine($"request: {pair.Key}: {string.Join(", ", pair.Value)}");
+                    httpClient.DefaultRequestHeaders.Add(pair.Key, string.Join(", ", pair.Value));
+                }
+
+                HttpResponseMessage response = await httpClient.GetAsync($"http://www.labaneilers.com{pathAndQuery}");
+                string responseBody = await response.Content.ReadAsStringAsync();
+                httpContext.Response.StatusCode = (int)response.StatusCode;
+
+                foreach (var pair in response.Headers) {
+                    httpContext.Response.Headers.Add(pair.Key, string.Join(", ", pair.Value));
+                }
+
+                await httpContext.Response.WriteAsync(responseBody);
+
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                httpContext.Response.StatusCode = 500;
+                await httpContext.Response.WriteAsync("Error: " + ex.Message);
+            }
+
             // Call the next middleware delegate in the pipeline 
-            await _next.Invoke(httpContext);
+            //await _next.Invoke(httpContext);
         }
     }
 }
